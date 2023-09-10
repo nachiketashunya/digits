77f757535d852975cc9943270149276443358a45
nachiketa <nachiketapurohit.2611@gmail.com>
2023-09-10 11:57:21 +0530

    Added gammas and C

--- exp.py
+++ exp.py
@@ -61,69 +61,29 @@ X, y = read_digits()
 # X_train, X_test, y_train, y_test = split_dataset(X, y, test_size=0.3)
 X_train, X_test, X_dev, y_train, y_test, y_dev = split_train_dev_test(X, y, test_size=0.3, dev_size=0.3)
 
+## Hyperparameter tunning
+gammas = [0.1, 0.001, 0.5, 1]
+cparams = [0.3, 1, 2, 0.7, 2]
+
 ## Use the preprocessed datas
 X_train = data_preprocess(X_train)
 X_test = data_preprocess(X_test)
+X_dev = data_preprocess(X_dev)
 
-model = train_model(X_train, y_train, {'gamma': 0.001}, model_type='svm')
+best_accur_sofar = -1
+for g in gammas:
+    for c in cparams:
+        cur_model = train_model(X_train, y_train, {'gamma': g, 'C' : c}, model_type='svm')
+        # Predict the value of the digit on the test subset
+        cur_accuracy = p_and_eval(cur_model, X_test, y_test)
 
-# Predict the value of the digit on the test subset
-# predicted = model.predict(X_test)
+        if cur_accuracy > best_accur_sofar:
+            print("New Best Accuracy : ", cur_accuracy)
+            best_accur_sofar = cur_accuracy
+            optimal_g = g
+            optimal_c = c 
+
+print(f"Training with optimal gamma {optimal_g} and optimal C {optimal_c}")
+model = train_model(X_train, y_train, {'gamma': optimal_g, 'C' : optimal_c}, model_type='svm')
 # Predict the value of the digit on the test subset
 predicted = p_and_eval(model, X_test, y_test)
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
-
-# _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-# for ax, image, prediction in zip(axes, X_test, predicted):
-#     ax.set_axis_off()
-#     image = image.reshape(8, 8)
-#     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-#     ax.set_title(f"Prediction: {prediction}")
-
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
-
-print(
-    f"Classification report for classifier {model}:\n"
-    f"{metrics.classification_report(y_test, predicted)}\n"
-)
-
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
-
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
-
-plt.show()
-
-###############################################################################
-# If the results from evaluating a classifier are stored in the form of a
-# :ref:`confusion matrix <confusion_matrix>` and not in terms of `y_true` and
-# `y_pred`, one can still build a :func:`~sklearn.metrics.classification_report`
-# as follows:
-
-
-# The ground truth and predicted lists
-y_true = []
-y_pred = []
-cm = disp.confusion_matrix
-
-# For each cell in the confusion matrix, add the corresponding ground truths
-# and predictions to the lists
-for gt in range(len(cm)):
-    for pred in range(len(cm)):
-        y_true += [gt] * cm[gt][pred]
-        y_pred += [pred] * cm[gt][pred]
-
-print(
-    "Classification report rebuilt from confusion matrix:\n"
-    f"{metrics.classification_report(y_true, y_pred)}\n"
-)
-
-print("")
-## Just a demo comment
